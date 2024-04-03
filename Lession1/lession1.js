bf35abc03fe4266389fc3edf11d7d10b7b8f48fe
Naitik Patel <120157810+naitikjpatel@users.noreply.github.com>
2024-04-03 13:45:45 +0530

    added file on 3 april

---
+++ Lession1/lession1.js
@@ -0,0 +1,38 @@
+/*
+a=10;
+// First Method to Print
+alert(a);
+
+//Second Method to Print
+console.log(a);
+
+//Third Method to Print
+print(a)
+document.write(a)
+
+*/
+
+
+//now we learn about the use Strict keyword
+
+"use strict"
+let b=10;
+var a=10;
+
+
+window.console.log("value of a  is "+a)
+console.log(b);
+
+// "use strict";
+// x = 3.14;       // This will cause an error because x is not declared
+
+
+//here we use two keyword 1)var and 2)let
+
+var a=100;
+var a=120;
+console.log("value of a  is "+a);
+
+
+// let a=11;
+// console.log(a);
