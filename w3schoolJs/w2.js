f7e82d3c8320ac77c0dd1d03a4fc8a510271f104
Naitik Patel <120157810+naitikjpatel@users.noreply.github.com>
2024-04-24 19:24:39 +0530

    added file on 24 april of year 24

---
+++ w3schoolJs/w2.js
@@ -0,0 +1,102 @@
+console.log("Out Topic Is Operators");
+
+/*
+Types of JavaScript Operators
+There are different types of JavaScript operators:
+
+Arithmetic Operators
+
+            +	Addition
+            -	Subtraction
+            *	Multiplication
+            **	Exponentiation (ES2016)
+            /	Division
+            %	Modulus (Division Remainder)
+            ++	Increment
+            --	Decrement
+
+
+Assignment Operators
+
+            =	x = y	x = y
+            +=	x += y	x = x + y
+            -=	x -= y	x = x - y
+            *=	x *= y	x = x * y
+            /=	x /= y	x = x / y
+            %=	x %= y	x = x % y
+            **=	x **= y	x = x ** y
+
+Comparison Operators
+            ==	equal to
+            ===	equal value and equal type
+            !=	not equal
+            !==	not equal value or not equal type
+            >	greater than
+            <	less than
+            >=	greater than or equal to
+            <=	less than or equal to
+            ?	ternary operator
+String Operators
+
+Logical Operators
+            &&	logical and
+            ||	logical or
+            !	logical not
+Bitwise Operators
+
+        &	AND	5 & 1	0101 & 0001	0001	 1
+        |	OR	5 | 1	0101 | 0001	0101	 5
+        ~	NOT	~ 5	 ~0101	1010	 10
+        ^	XOR	5 ^ 1	0101 ^ 0001	0100	 4
+        <<	left shift	5 << 1	0101 << 1	1010	 10
+        >>	right shift	5 >> 1	0101 >> 1	0010	  2
+        >>>	unsigned right shift	5 >>> 1	0101 >>> 1	0010	  2
+Ternary Operators
+Type Operators
+*/
+
+
+/**
+ Data Types:
+ JavaScript has 8 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object
+
+
+The Object Datatype
+The object data type can contain:
+
+1. An object
+2. An array
+3. A date
+ */
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+// Note : A javascript Varible can hold any type of data.
